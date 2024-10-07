47b21f25ae3526869c6fc3d4f5284dfc7c96fb13
joanitha <joanitha@securly.com>
2024-10-06 20:57:30 -0700

    Optimize and css updates

--- src/App.js
+++ src/App.js
@@ -16,9 +16,6 @@ function App() {
 
   return (
     <div className="app">
-      <header className="app-header">
-        <h1>WeatherInTown</h1>
-      </header>
       <main>
         <div className="box">
 
--- src/Temperature/Temperature.js
+++ src/Temperature/Temperature.js
@@ -9,7 +9,7 @@ export function Temperature({tempF, tempC, icon, description, city, localTime}) 
         <div>
             <img src={icon} alt={description} className="weather-icon"/>
 
-            <h2>{ tempC } <span className="unit">&deg;C</span> <span className="divider"></span> { tempF } <span className='unit'>&deg;F</span></h2>
+            <h2 className='temperature'>{ tempC } <span className="unit">&deg;C</span> <span className="divider"></span> { tempF } <span className='unit'>&deg;F</span></h2>
             <p className="capitalize my-0">{ description }</p>
             <p className='info'><AccessTimeOutlinedIcon fontSize="small" className="icon"/>{ localTime }</p>
             <p className='info'><LocationOnOutlined fontSize="small" className="icon"/>{city}</p>
